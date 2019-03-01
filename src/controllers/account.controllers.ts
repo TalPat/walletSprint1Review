3b938fb1cae21aff56a54a1e3cbd2b130ef6722f
jadinek <jkruger@student.wethinkcode.co.za>
2019-03-01 17:36:17 +0200

    added account controller

---
+++ src/controllers/account.controllers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import * as accountDom from './datalayer/account.domain'
+import * as accInterface from '../interfaces/account.interface'
+
+export function createAccount (req: Request, res: Response) {
+  const data = req.body
+  const account: accInterface.Account = {
+    accountID: null,
+    accountName: data.accountName,
+    ownerUserID: 21,
+    balance: data.balance }
+  if (!accountDom.createAccount(account)) {
+    res.status(500).send('Account creation failed')
+  } else {
+    res.send('Account created')
+  }
+
+    // valiidate
+    // if validate fails, return 'invalid inputs'
+    // if validate suceeds call createAccountDom
+    // recieve 1?0 fron createAccountDom
+    // if 1 return 'created'
+    // if 0 retutn 'failed to create'
+
+}
