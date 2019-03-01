63dbbec0bc574b4d3453b7093407ec2e1259eac3
talpat <talonpatterson94@gmail.com>
2019-03-01 16:53:25 +0200

    add account dl

---
+++ src/datalayer/account.domain.ts
@@ -0,0 +1,40 @@
+import * as dbFunctions from './db'
+import { Account } from '../interfaces/accout.interface'
+
+// input object:
+// {
+//   action: 'create'/'read'/'update'/'delete',
+//   values: {accountName: '', balance: '', ownerUserID: ''} #of type ' account'
+// }
+
+// function to handle adding account
+export function createAccount (account: Account, callback: (result: object) => void) {
+  const sql = `INSERT INTO accounts (accountName, balance, ownerUserID) VALUES ('${account.accountName}', ${account.balance}, ${account.ownerUserID})`
+  dbFunctions.query(sql, function (err: object) {
+    if (err) {
+      callback({ error: true })
+    } else {
+      callback({ error: false })
+    }
+  })
+}
+
+// export function readAccountByID () {
+
+// }
+
+// export function readAcountsByName () {
+
+// }
+
+// export function readAccountsByOwnerID () {
+
+// }
+
+// export function updateAccount () {
+
+// }
+
+// export function deleteAccount () {
+
+// }
